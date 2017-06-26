f9f99d0736b5b766b49fa7d058718ec9e6dffe0f
Kirill Matrosov <kdmatrosov@gmail.com>
2017-06-26 16:12:00 +0300

    refactor. routing structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Test from '@/components/Test.vue';
-import NotFoundComponent from '@/common/NotFoundComponent.vue';
+import NotFoundComponent from '@/routes/common/404.vue';
 Vue.use(Router);
 
 export default new Router({
