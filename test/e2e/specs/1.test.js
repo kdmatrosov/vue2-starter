1b3fc25f8c28b486581ca4d861766837ebca36f9
Kirill Matrosov <kdmatrosov@gmail.com>
2017-07-04 18:33:16 +0300

    e2e stable

--- test/e2e/specs/1.test.js
+++ test/e2e/specs/1.test.js
@@ -3,7 +3,11 @@ const devServer = require('../../../webpack/devServer');
 
 fixture(`Getting Started`)
 // Load the URL your development server runs on.
-    .page(`http://localhost:${devServer.port}`);
+    .page(`http://localhost:${devServer.port}/#/404`);
 
 test('test index', async t => {
+    await t.click('button');
+    const getLocation = ClientFunction(() => document.location.href);
+    await t.expect(getLocation()).notContains('/#/404');
+    await t.click('h1');
 });
