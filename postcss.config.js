dd07f48e6f6ee01f5102b977aa7a8733790e746c
Kirill Matrosov <kdmatrosov@gmail.com>
2017-08-01 14:49:50 +0300

    update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,27 @@
+const resolve = require('./webpack/resolve');
+const MAP = {
+    '@styles': resolve('src/styles')
+};
+
 module.exports = {
     plugins: {
-        'postcss-import': {},
+        'postcss-import': {
+            resolve: function (imp, base, opt) {
+                const slash = ~base.indexOf('/') ? '/' : '\\';
+                let path = base + slash + imp;
+                for (let key in MAP) {
+                    if (~imp.indexOf(key)) {
+                        path = MAP[key] + imp.split(key)[1] || '';
+                    }
+                }
+                return path;
+            }
+        },
         'postcss-cssnext': {
             browsers: ['last 5 versions', '> 5%']
         },
         'precss': {},
-        'postcss-simple-vars': {},
-        'postcss-mixins': {}
+        'postcss-calc': {},
+        'postcss-each': {}
     }
 };
